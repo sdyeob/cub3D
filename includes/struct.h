61b586974977b7b5ac07f5b835988e2e6983cb35
dongyshi <dongyshi@student.42seoul.kr>
2023-07-07 21:25:47 +0900

    [FEAT] structures/#2

---
+++ includes/struct.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dongyshi <dongyshi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 20:54:59 by dongyshi          #+#    #+#             */
+/*   Updated: 2023/07/07 21:24:25 by dongyshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct s_vec
+{
+    double  x;
+    double  y;
+}   t_vec;
+
+typedef struct s_identifier
+{
+    char    *n;
+    char    *w;
+    char    *e;
+    char    *s;
+    int     f[3];
+    int     c[3];
+}   t_identifier;
+
+typedef struct s_map_inf {
+    t_identifier    identifier;
+    char            **map;
+    int             m_width;
+    int             m_height;
+    int             player_loc_x;
+    int             player_loc_y;
+    char            player_direct; // 방위.
+}   t_map_inf;
+
+#endif
